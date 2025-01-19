8623d8146c7408a55c76497fa0fa319993fe4076
limwoobin <drogba02@naver.com>
2025-01-19 21:39:00 +0900

    feat: batch metrics init

---
+++ batch-metrics/src/main/java/org/example/batchmetrics/BatchMetricsApplication.java
@@ -0,0 +1,13 @@
+package org.example.batchmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMetricsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BatchMetricsApplication.class, args);
+  }
+
+}
---
+++ batch-metrics/src/test/java/org/example/batchmetrics/BatchMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.batchmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchMetricsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
